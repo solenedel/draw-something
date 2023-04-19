6ec4204eccea2713c067c1e01ffc7ea8f993442a
Solène <81546747+solenedel@users.noreply.github.com>
2023-04-19 10:51:06 -0700

    work on useDraw hook

--- hooks/useDraw.ts
+++ hooks/useDraw.ts
@@ -5,9 +5,27 @@ export const useDraw = () => {
   useEffect(() => {
     const handler = (e: MouseEvent) => {
       console.log({ x: e.clientX, y: e.clientY });
+      const currentPoint = computePointInCanvas(e);
+
+      const context = canvasRef.current?.getContext('2d');
+      if (!context || !currentPoint) return;
     };
+
+    const computePointInCanvas = (e: MouseEvent) => {
+      const canvas = canvasRef.current;
+
+      if (!canvas) return;
+
+      const rect = canvas.getBoundingClientRect();
+      const x = e.clientX - rect.left;
+      const y = e.clientY - rect.top;
+      return { x, y };
+    };
+
+    // add event listners
     canvasRef.current?.addEventListener('mousemove', handler);
 
+    // remove event listners
     // clean up
     return () => canvasRef.current?.addEventListener('mousemove', handler);
   }, []);
